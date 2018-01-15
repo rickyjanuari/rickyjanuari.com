b7b22b9ae04e93a2a1b03790ec04b3a2d7326079
rickyjanuari <ricky010193@gmail.com>
2018-01-15 10:13:10 +0700

    portfolio grid

---
+++ src/pages/portfolio/automarket/index.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import Helmet from "react-helmet";
+
+class Automarket extends Component {
+  render() {
+    return (
+        <div className="container contact-container">
+            
+            <div className="row">
+                <div className="portfolio-item col-xs-12 col-sm-6 col-md-4">
+                    <div className="portfolio-item-list">
+                        1
+                    </div>
+                </div>
+            </div>  
+        </div>
+    );
+  }
+}
+
+export default Automarket;
--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -2,14 +2,36 @@ import React, { Component } from "react";
 import Helmet from "react-helmet";
 import Contact from "components/Contact/Contact";
 import config from "../../../data/SiteConfig";
-
+import "./styles.scss";
 class PortfolioIndex extends Component {
   render() {
     return (
-      <div className="container contact-container">
-        <Helmet title={`Contact | ${config.siteTitle}`} />
-        <h1>404</h1>
-      </div>
+        <div className="container contact-container">
+            <Helmet title={`Portfolio | ${config.siteTitle}`} />
+            <h2>Portfolio</h2>
+            <div className="row">
+                <div className="portfolio-item col-xs-12 col-sm-6 col-md-4">
+                    <div className="portfolio-item-list">
+                        1
+                    </div>
+                </div>
+                <div className="portfolio-item col-xs-12 col-sm-6 col-md-4">
+                    <div className="portfolio-item-list">
+                        1
+                    </div>
+                </div>
+                <div className="portfolio-item col-xs-12 col-sm-6 col-md-4">
+                    <div className="portfolio-item-list">
+                        1
+                    </div>
+                </div>
+                <div className="portfolio-item col-xs-12 col-sm-6 col-md-4">
+                    <div className="portfolio-item-list">
+                        1
+                    </div>
+                </div>
+            </div>  
+        </div>
     );
   }
 }
