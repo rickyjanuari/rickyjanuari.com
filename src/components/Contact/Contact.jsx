5083e050e574ba91b1a0c479a603f4624b3dd1d4
rickyjanuari <ricky010193@gmail.com>
2018-01-17 10:14:43 +0700

    contact page

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,18 +1,27 @@
 import React, { Component } from "react";
-import "./Contact.css";
-import MessengerCustomerChat from 'react-messenger-customer-chat';
+import "./Contact.scss";
+import Email from 'react-icons/lib/ti/mail';
+import Facebook from 'react-icons/lib/ti/social-facebook';
+import Twitter from 'react-icons/lib/ti/social-twitter';
+import Linkedin from 'react-icons/lib/ti/social-linkedin';
+import Skype from 'react-icons/lib/ti/social-skype';
 
 class Contact extends Component {
   render() {
     return (
       <div className="contact">
-        <h1>
-            Ini Halaman Contact
-        </h1>
-        <MessengerCustomerChat
-          pageId="1456719061293720"
-          appId="159308587468711"
-        />
+        <div className="envelope">
+					<h3 className="contact-t">Contact / Hire Me</h3>
+					<h3 className="contact-line contact-t"></h3>
+					<div className="centered">
+							<a id="mailme" href="mailto:ricky010193@gmail.com"><Email className="icon-social"/></a>
+							<a id="fb" href="http://www.facebook.com/rickyjanuaris" target="blank_"><Facebook className="icon-social"/></a>
+							<a id="linkedin" href="https://www.linkedin.com/in/rickyjanuari/" target="blank_"><Linkedin className="icon-social"/></a>
+              <a id="linkedin" href="skype:rickyjanuari?userinfo" target="blank_"><Skype className="icon-social"/></a>
+						
+					</div>				
+					<h4 className="contact-note">Always interested in new projects, Just Send me an email</h4>
+				</div>
       </div>
     );
   }
