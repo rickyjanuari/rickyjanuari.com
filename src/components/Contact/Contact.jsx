28f29883a2b45c94377caa5fdaf0f534952d3854
rickyjanuari <ricky010193@gmail.com>
2018-01-17 10:35:07 +0700

    test

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
 import "./Contact.scss";
-import Email from 'react-icons/lib/ti/mail';
-import Facebook from 'react-icons/lib/ti/social-facebook';
-import Twitter from 'react-icons/lib/ti/social-twitter';
-import Linkedin from 'react-icons/lib/ti/social-linkedin';
-import Skype from 'react-icons/lib/ti/social-skype';
-
+import Email from 'react-icons/lib/fa/envelope-square';
+import Facebook from 'react-icons/lib/fa/facebook-square';
+import Twitter from 'react-icons/lib/fa/twitter-square';
+import Linkedin from 'react-icons/lib/fa/linkedin-square';
+import Skype from 'react-icons/lib/fa/skype';
+import WA from 'react-icons/lib/fa/whatsapp';
 class Contact extends Component {
   render() {
     return (
@@ -15,9 +15,10 @@ class Contact extends Component {
 					<h3 className="contact-line contact-t"></h3>
 					<div className="centered">
 							<a id="mailme" href="mailto:ricky010193@gmail.com"><Email className="icon-social"/></a>
-							<a id="fb" href="http://www.facebook.com/rickyjanuaris" target="blank_"><Facebook className="icon-social"/></a>
+							<a id="fb" href="http://me.me/rickyjanuaris" target="blank_"><Facebook className="icon-social"/></a>
 							<a id="linkedin" href="https://www.linkedin.com/in/rickyjanuari/" target="blank_"><Linkedin className="icon-social"/></a>
-              <a id="linkedin" href="skype:rickyjanuari?userinfo" target="blank_"><Skype className="icon-social"/></a>
+              <a id="linkedin" href="http://m.me/rickyjanuaris" target="blank_"><Skype className="icon-social"/></a>
+              <a id="linkedin" href="https://api.whatsapp.com/send?phone=6285759621854" target="blank_"><WA className="icon-social"/></a>
 						
 					</div>				
 					<h4 className="contact-note">Always interested in new projects, Just Send me an email</h4>
