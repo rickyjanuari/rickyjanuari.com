f9717f6ca9cc292b6ce3432050f54022fc4aa905
rickyjanuari <ricky010193@gmail.com>
2018-01-17 10:45:27 +0700

    update about

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,9 +4,9 @@ import "./About.css";
 class About extends Component {
   render() {
     return (
-      <div className="content-container about">
+      <div className="container about">
         <div className="about__left">
-            <h2>About Me</h2>
+            <h1>About Me</h1>
             <p>I’m Ricky Januari a Freelance Front-end Developer &amp; Designer based in Bandung - Indonesia. I have over 2 years of experience designing &amp; building websites.</p>
             <p>I care about an open, accessible, fast, responsive, and well-built web that puts people first. When I’m not working toward that, I’m probably reading, riding my bike</p>
         </div>
@@ -27,14 +27,24 @@ class About extends Component {
             <li>Communication</li>
             <li>Learning</li>
           </ul>
-  <h3 className="h4 mb-0">I’m Getting Better With</h3>
-  <ul>
-    <li>Performance</li>
-    <li>Build Tools (Gulp, Webpack)</li>
-    <li>Typography</li>
-    <li>Color Theory</li>
-    <li>React</li>
-  </ul>
+          <h3 className="h4 mb-0">I’m Getting Better With</h3>
+          <ul>
+            <li>Performance</li>
+            <li>Build Tools (Gulp, Webpack)</li>
+            <li>Typography</li>
+            <li>Color Theory</li>
+            <li>React</li>
+          </ul>
+
+          <h2>Work Experience</h2>
+          <h3>Front-end Web Developers - agensidigital.com, Bandung - Indonesia</h3>
+          <p>Work Remotely from home <br/> August 2016 - December 2017</p>
+          <h4>My responsibilities include:</h4>
+          <ul>
+            <li>Bringing a designer’s concept to life with HTML, CSS, and JavaScript</li>
+            <li>Building reusable code and libraries for future use</li>
+            <li>Implementing responsive design</li>
+          </ul>
         </div>
       </div>
     );
