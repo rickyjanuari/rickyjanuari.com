3dec8eef9032f208af8fe67e78602976ee3d3b29
rickyjanuari <ricky010193@gmail.com>
2018-04-16 15:32:57 +0700

    test

--- src/pages/work/index.jsx
+++ src/pages/work/index.jsx
@@ -14,142 +14,172 @@ class WorkIndex extends Component {
             <h2>Work</h2>
             <p>Here are some projects I've worked on as Front-end Developer</p>
             <Tabs
+                className="Tabs"
               tabs={[
                 {
                   children: () => (
                     <div className="row">
-                <div className="col-xs-12 col-md-6">
-                    <div className="work__wrap">
-                        <img className="work__img" src="https://res.cloudinary.com/codesains/image/upload/c_scale,w_500/v1521419869/mbw_zraobv.jpg" />
-                        <div className="work__description">
-                            <a className="work_name" href="https://mbw.netlify.com/" target="_blank"><h4>MBW</h4></a>
-                            <span className="c-portfolio-item__tags">
-                                Frontend Developer, PSD to HTML , Responsive Design
-                            </span>
+                        <div className="col-xs-12 col-md-6">
+                            <div className="work__wrap">
+                                <img className="work__img" src="https://res.cloudinary.com/codesains/image/upload/c_scale,w_500/v1521419869/mbw_zraobv.jpg" />
+                                <div className="work__description">
+                                    <a className="work_name" href="https://mbw.netlify.com/" target="_blank"><h4>MBW</h4></a>
+                                    <span className="c-portfolio-item__tags">
+                                        Frontend Developer, PSD to HTML , Responsive Design
+                                    </span>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="col-xs-12 col-md-6">
-                    <div className="work__wrap">
-                        <img className="work__img" src="https://res.cloudinary.com/codesains/image/upload/c_scale,w_500/v1516099596/rickyjanuari.com/querella.jpg" />
-                        <div className="work__description">
-                            <a className="work_name" href="https://querella.herokuapp.com/" target="_blank"><h4>querella</h4></a>
-                            <span className="c-portfolio-item__tags">
-                                Frontend Developer, PSD to HTML , Responsive Design
-                            </span>
+                        <div className="col-xs-12 col-md-6">
+                            <div className="work__wrap">
+                                <img className="work__img" src="https://res.cloudinary.com/codesains/image/upload/c_scale,w_500/v1516099596/rickyjanuari.com/querella.jpg" />
+                                <div className="work__description">
+                                    <a className="work_name" href="https://querella.herokuapp.com/" target="_blank"><h4>querella</h4></a>
+                                    <span className="c-portfolio-item__tags">
+                                        Frontend Developer, PSD to HTML , Responsive Design
+                                    </span>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="col-xs-12 col-md-6">
-                    <div className="work__wrap">
-                        <img className="work__img" src="https://res.cloudinary.com/codesains/image/upload/c_scale,w_500/v1516099596/rickyjanuari.com/tihotline.jpg" />
-                        <div className="work__description">
-                            <a className="work_name" href="https://tihotline.com/" target="_blank"><h4>tihotline.com</h4></a>
-                            <span className="c-portfolio-item__tags">
-                                Frontend Developer, PSD to HTML , Responsive Design
-                            </span>
+                        <div className="col-xs-12 col-md-6">
+                            <div className="work__wrap">
+                                <img className="work__img" src="https://res.cloudinary.com/codesains/image/upload/c_scale,w_500/v1516099596/rickyjanuari.com/tihotline.jpg" />
+                                <div className="work__description">
+                                    <a className="work_name" href="https://tihotline.com/" target="_blank"><h4>tihotline.com</h4></a>
+                                    <span className="c-portfolio-item__tags">
+                                        Frontend Developer, PSD to HTML , Responsive Design
+                                    </span>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="col-xs-12 col-md-6">
-                    <div className="work__wrap">
-                        <img className="work__img" src="https://res.cloudinary.com/codesains/image/upload/c_scale,w_500/v1516099596/rickyjanuari.com/trendyagent.jpg" />
-                        <div className="work__description">
-                            <a className="work_name" href="http://agent.tihotline.com/" target="_blank"><h4>Trendy Agent</h4></a>
-                            <span className="c-portfolio-item__tags">
-                                Frontend Developer, PSD to HTML , Responsive Design
-                            </span>
+                        <div className="col-xs-12 col-md-6">
+                            <div className="work__wrap">
+                                <img className="work__img" src="https://res.cloudinary.com/codesains/image/upload/c_scale,w_500/v1516099596/rickyjanuari.com/trendyagent.jpg" />
+                                <div className="work__description">
+                                    <a className="work_name" href="http://agent.tihotline.com/" target="_blank"><h4>Trendy Agent</h4></a>
+                                    <span className="c-portfolio-item__tags">
+                                        Frontend Developer, PSD to HTML , Responsive Design
+                                    </span>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="col-xs-12 col-md-6">
-                    <div className="work__wrap">
-                        <img className="work__img" src="https://res.cloudinary.com/codesains/image/upload/c_scale,w_500/v1516099597/rickyjanuari.com/jpauc.jpg" />
-                        <div className="work__description">
-                            <a className="work_name" href="http://jpauc.com" target="_blank"><h4>jpauc.com</h4></a>
-                            <span className="c-portfolio-item__tags">
-                                Frontend modifications
-                            </span>
+                        <div className="col-xs-12 col-md-6">
+                            <div className="work__wrap">
+                                <img className="work__img" src="https://res.cloudinary.com/codesains/image/upload/c_scale,w_500/v1516099597/rickyjanuari.com/jpauc.jpg" />
+                                <div className="work__description">
+                                    <a className="work_name" href="http://jpauc.com" target="_blank"><h4>jpauc.com</h4></a>
+                                    <span className="c-portfolio-item__tags">
+                                        Frontend modifications
+                                    </span>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="col-xs-12 col-md-6">
-                    <div className="work__wrap">
-                        <img className="work__img" src="https://res.cloudinary.com/codesains/image/upload/c_scale,w_500/v1516099597/rickyjanuari.com/automarket.jpg" />
-                        <div className="work__description">
-                            <a className="work_name" href="http://market.jpauc.com" target="_blank"><h4>market.jpauc.com</h4></a>
-                            <span className="c-portfolio-item__tags">
-                                Frontend Developer, PSD to HTML , Responsive Design All Page
-                            </span>
+                        <div className="col-xs-12 col-md-6">
+                            <div className="work__wrap">
+                                <img className="work__img" src="https://res.cloudinary.com/codesains/image/upload/c_scale,w_500/v1516099597/rickyjanuari.com/automarket.jpg" />
+                                <div className="work__description">
+                                    <a className="work_name" href="http://market.jpauc.com" target="_blank"><h4>market.jpauc.com</h4></a>
+                                    <span className="c-portfolio-item__tags">
+                                        Frontend Developer, PSD to HTML , Responsive Design All Page
+                                    </span>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="col-xs-12 col-md-6">
-                    <div className="work__wrap">
-                        <img className="work__img" src="https://res.cloudinary.com/codesains/image/upload/c_scale,w_500/v1516099596/rickyjanuari.com/hmd.jpg" />
-                        <div className="work__description">
-                            <a className="work_name" href="http://apmotor-jpauc.com/auction" target="_blank"><h4>apmotor-jpauc.com</h4></a>
-                            <span className="c-portfolio-item__tags">
-                                Frontend modifications Wesbite Agent From web jpauc.com
-                            </span>
+                        <div className="col-xs-12 col-md-6">
+                            <div className="work__wrap">
+                                <img className="work__img" src="https://res.cloudinary.com/codesains/image/upload/c_scale,w_500/v1516099596/rickyjanuari.com/hmd.jpg" />
+                                <div className="work__description">
+                                    <a className="work_name" href="http://apmotor-jpauc.com/auction" target="_blank"><h4>apmotor-jpauc.com</h4></a>
+                                    <span className="c-portfolio-item__tags">
+                                        Frontend modifications Wesbite Agent From web jpauc.com
+                                    </span>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="col-xs-12 col-md-6">
-                    <div className="work__wrap">
-                        <img className="work__img" src="https://res.cloudinary.com/codesains/image/upload/c_scale,w_500/v1516099597/rickyjanuari.com/crauc.jpg" />
-                        <div className="work__description">
-                            <a className="work_name" href="http://crauc.com/" target="_blank"><h4>crauc.com</h4></a>
-                            <span className="c-portfolio-item__tags">
-                                Frontend modifications Wesbite Agent From web jpauc.com
-                            </span>
+                        <div className="col-xs-12 col-md-6">
+                            <div className="work__wrap">
+                                <img className="work__img" src="https://res.cloudinary.com/codesains/image/upload/c_scale,w_500/v1516099597/rickyjanuari.com/crauc.jpg" />
+                                <div className="work__description">
+                                    <a className="work_name" href="http://crauc.com/" target="_blank"><h4>crauc.com</h4></a>
+                                    <span className="c-portfolio-item__tags">
+                                        Frontend modifications Wesbite Agent From web jpauc.com
+                                    </span>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="col-xs-12 col-md-6">
-                    <div className="work__wrap">
-                        <img className="work__img" src="https://res.cloudinary.com/codesains/image/upload/c_scale,w_500/v1516099597/rickyjanuari.com/trendy168.jpg" />
-                        <div className="work__description">
-                            <a className="work_name" href="http://trendy168.co.jp" target="_blank"><h4>trendy168.co.jp</h4></a>
-                            <span className="c-portfolio-item__tags">
-                                Frontend Developer With Wordpress
-                            </span>
+                        <div className="col-xs-12 col-md-6">
+                            <div className="work__wrap">
+                                <img className="work__img" src="https://res.cloudinary.com/codesains/image/upload/c_scale,w_500/v1516099597/rickyjanuari.com/trendy168.jpg" />
+                                <div className="work__description">
+                                    <a className="work_name" href="http://trendy168.co.jp" target="_blank"><h4>trendy168.co.jp</h4></a>
+                                    <span className="c-portfolio-item__tags">
+                                        Frontend Developer With Wordpress
+                                    </span>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="col-xs-12 col-md-6">
-                    <div className="work__wrap">
-                        <img className="work__img" src="http://res.cloudinary.com/codesains/image/upload/c_scale,w_500/v1516099598/rickyjanuari.com/bermuda.jpg" />
-                        <div className="work__description">
-                            <a className="work_name" href="http://bermuda-niaga.com/" target="_blank"><h4>bermuda-niaga.com</h4></a>
-                            <span className="c-portfolio-item__tags">
-                                Frontend modifications Wesbite Agent  jpauc.com
-                            </span>
+                        <div className="col-xs-12 col-md-6">
+                            <div className="work__wrap">
+                                <img className="work__img" src="http://res.cloudinary.com/codesains/image/upload/c_scale,w_500/v1516099598/rickyjanuari.com/bermuda.jpg" />
+                                <div className="work__description">
+                                    <a className="work_name" href="http://bermuda-niaga.com/" target="_blank"><h4>bermuda-niaga.com</h4></a>
+                                    <span className="c-portfolio-item__tags">
+                                        Frontend modifications Wesbite Agent  jpauc.com
+                                    </span>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-                <div className="col-xs-12 col-md-6">
-                    <div className="work__wrap">
-                        <img className="work__img" src="https://res.cloudinary.com/codesains/image/upload/c_scale,w_500/v1516099596/rickyjanuari.com/carsway.jpg" />
-                        <div className="work__description">
-                            <a className="work_name" href="http://www.carsway.com.hk/" target="_blank"><h4>carsway.com.hk/</h4></a>
-                            <span className="c-portfolio-item__tags">
-                                Frontend modifications Wesbite Agent From web jpauc.com
-                            </span>
+                        <div className="col-xs-12 col-md-6">
+                            <div className="work__wrap">
+                                <img className="work__img" src="https://res.cloudinary.com/codesains/image/upload/c_scale,w_500/v1516099596/rickyjanuari.com/carsway.jpg" />
+                                <div className="work__description">
+                                    <a className="work_name" href="http://www.carsway.com.hk/" target="_blank"><h4>carsway.com.hk/</h4></a>
+                                    <span className="c-portfolio-item__tags">
+                                        Frontend modifications Wesbite Agent From web jpauc.com
+                                    </span>
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            </div> 
+                    </div> 
                   ),
                   displayName: 'Wesbite'
                 },
                 {
                   children: () => (
-                    <div>
-                      This is the second tab content
+                    <div className="row">
+                        <div className="col-xs-12 col-md-6">
+                            <div className="work__wrap">
+                                <img className="work__img" src="https://res.cloudinary.com/codesains/image/upload/c_scale,w_500/v1521419869/mbw_zraobv.jpg" />
+                                <div className="work__description">
+                                    <a className="work_name" href="https://mbw.netlify.com/" target="_blank"><h4>MBW</h4></a>
+                                    <span className="c-portfolio-item__tags">
+                                        Frontend Developer, PSD to HTML , Responsive Design
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-xs-12 col-md-6">
+                            <div className="work__wrap">
+                                <img className="work__img" src="https://res.cloudinary.com/codesains/image/upload/c_scale,w_500/v1516099596/rickyjanuari.com/querella.jpg" />
+                                <div className="work__description">
+                                    <a className="work_name" href="https://querella.herokuapp.com/" target="_blank"><h4>querella</h4></a>
+                                    <span className="c-portfolio-item__tags">
+                                        Frontend Developer, PSD to HTML , Responsive Design
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                   ),
-                  displayName: 'Mobile'
+                  displayName: 'PSD to HTML'
+                },
+                {
+                  children: () => (
+                    <div className="row">
+                        React Native
+                    </div>
+                  ),
+                  displayName: 'React Native'
                 }
               ]}
             />
